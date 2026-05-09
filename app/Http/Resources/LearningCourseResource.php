a7e5106f83d33e0b1d0d90ebf63a461dff963e02
tuhu02 <230411100096@student.trunojoyo.ac.id>
2026-05-09 17:51:55 +0700

    update

--- app/Http/Resources/LearningCourseResource.php
+++ app/Http/Resources/LearningCourseResource.php
@@ -4,6 +4,8 @@ namespace App\Http\Resources;
 
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
+use App\Models\Module;
+
 
 class LearningCourseResource extends JsonResource
 {
